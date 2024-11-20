ef4c8e18dcd5b65696e4c762935fb11dbb50468a
pnrmaja <ponauerszilvi1@gmail.com>
2024-11-20 17:50:03 +0100

    valasztas validalasa

--- src/main/java/com/mycompany/kopapirollo/KoPapirOllo.java
+++ src/main/java/com/mycompany/kopapirollo/KoPapirOllo.java
@@ -13,8 +13,11 @@ public class KoPapirOllo {
     public static void main(String[] args) {
        String valasztas = Adatbekeres.beker("ko/papir/ollo/kilepes");
        System.out.println("Válaszod: " + valasztas);
-       
-       
-        
+       boolean helyesE=helyesValasztas(valasztas);
+       System.out.println( helyesE);
+     
+    }
+    public static boolean helyesValasztas(String valasztas){
+        return "ko".equals(valasztas) || "papir".equals(valasztas) || "ollo".equals(valasztas) || "kilepes".equals(valasztas);
     }
 }
