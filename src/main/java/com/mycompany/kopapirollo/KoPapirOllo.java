592b5319662140ed35a92ea33a6c45141d243a15
pnrmaja <ponauerszilvi1@gmail.com>
2024-11-20 18:14:52 +0100

    eldonto logika

--- src/main/java/com/mycompany/kopapirollo/KoPapirOllo.java
+++ src/main/java/com/mycompany/kopapirollo/KoPapirOllo.java
@@ -11,13 +11,34 @@ package com.mycompany.kopapirollo;
 public class KoPapirOllo {
 
     public static void main(String[] args) {
-       String valasztas = Adatbekeres.beker("ko/papir/ollo/kilepes");
-       System.out.println("Válaszod: " + valasztas);
-       boolean helyesE=helyesValasztas(valasztas);
-       System.out.println( helyesE);
+        jatek();
      
     }
     public static boolean helyesValasztas(String valasztas){
         return "ko".equals(valasztas) || "papir".equals(valasztas) || "ollo".equals(valasztas) || "kilepes".equals(valasztas);
+    }
+    public static String dontes(String jatekos, String gep){
+        if (jatekos.equals(gep)){
+            return "Dontetlen";
+        }
+        if (    ("ko".equals(jatekos) && "papir".equals(gep) ) ||
+                ("papir".equals(jatekos) && "ollo".equals(gep)) ||
+                ("ollo".equals(jatekos) && "ko".equals(gep))){
+            return "A gep nyert!";
+        }
+        return "A jatekos nyert!:)";
+        
+    }
+    public static void jatek(){
+        String valasztas =Adatbekeres.beker("ko/papir/ollo/kilepes");
+        
+        while (!helyesValasztas(valasztas)) {
+            System.out.println("Hibás bemenet! Adj meg újat");
+            valasztas =Adatbekeres.beker("ko/papir/ollo/kilepes");  
+        }
+        String dontes = dontes(valasztas,"ko");
+        System.out.println(dontes);
+        
+    
     }
 }
